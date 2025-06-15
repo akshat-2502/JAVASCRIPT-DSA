7cc1a84b215a17472ccb6d5ad06da4544f1bba90
akshat-2502 <khulbeakshat@gmail.com>
2025-06-15 23:45:36 +0530

    negative number done

--- WARM-UP/searchElement.js
+++ WARM-UP/searchElement.js
@@ -1,14 +1,29 @@
-const searchElement = (arr, num) => {
+// const searchElement = (arr, num) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === num) {
+//       console.log(i);
+//       return;
+//     }
+//   }
+//   console.log("-1");
+//   return;
+// };
+
+// let arr = [4, 10, 0, 3, 22, 34, 1, 9, 11];
+
+// searchElement(arr, 34);
+
+function negativeNumber(arr) {
+  let counter = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === num) {
-      console.log(i);
-      return;
+    if (arr[i] < 0) {
+      counter += 1;
     }
   }
-  console.log("-1");
-  return;
-};
+  return counter;
+}
 
-let arr = [4, 10, 0, 3, 22, 34, 1, 9, 11];
+let arr1 = [1, , 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-searchElement(arr, 34);
+let numbers = negativeNumber(arr1);
+console.log(numbers);
---
+++ WARM-UP/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+searchElement(arr, 34);
